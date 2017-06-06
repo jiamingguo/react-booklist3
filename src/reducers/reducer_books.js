a01925dd2d7d44c22f4eff44cdbb84e98f72b514
jiamingguo <gjm1993217@gmail.com>
2017-06-06 00:57:51 -0700

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,19 @@
+/**
+ * Created by gjm19_000 on 6/6/2017.
+ */
+/*
+Reducers: return a piece of application state
+
+Application state: {activebooks:xxx, allbooks:xxx}
+
+So corresponding two reducers for each field in application state
+ */
+
+export default function () {
+    return [
+        {title: 'Javascript: the Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
